cf7119c6bfaa9c39f7b61fd8d58f5ec9c253b080
discoduro <david.diaz637@misena.edu.co>
2023-06-03 15:20:12 -0500

    prueba

--- models/DataBase.php
+++ models/DataBase.php
@@ -1,12 +1,29 @@
 <?php
+    // class DataBase{
+    //     public static function connection(){
+    //         $servername = "localhost";
+    //         $database = "dbappwebinv";
+    //         $username = "root";
+    //         $password = "";
+	// 		$pdo = new PDO("mysql:host=$servername;dbname=$database;charset=utf8",$username,$password);
+	// 		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	// 		return $pdo;
+	// 	}
+	// }
+
+
     class DataBase{
         public static function connection(){
-            $servername = "localhost";
+            $hostname = "appwebsrv.mysql.database.azure.com";
+            $port = "3306";
             $database = "dbappwebinv";
-            $username = "root";
-            $password = "";
-			$pdo = new PDO("mysql:host=$servername;dbname=$database;charset=utf8",$username,$password);
+            $username = "soporte";
+            $password = "Help.2023*";
+            $options = array(
+                PDO::MYSQL_ATTR_SSL_CA => 'assets/db/DigiCertGlobalRootCA.crt.pem'
+            );
+			$pdo = new PDO("mysql:host=$hostname;port=$port;dbname=$database;charset=utf8",$username,$password,$options);
 			$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			return $pdo;
 		}
-	}
+	}    
